b2501211a23caf1faa393a5e6184d697e93a8d03
Taras Lisovych <taras.lisovych@onapp.com>
2022-06-22 20:28:29 +0300

    Added Key Pair to the module

--- aws_network/main.tf
+++ aws_network/main.tf
@@ -102,7 +102,13 @@ resource "aws_key_pair" "kp" {
   key_name   = "${var.env} key" # Create a "myKey" to AWS!!
   public_key = tls_private_key.pk.public_key_openssh
 
-  provisioner "local-exec" { # Create a "myKey.pem" to your computer!!
+  /*  provisioner "local-exec" { # Create a "myKey.pem" to your computer!!
     command = "echo '${tls_private_key.pk.private_key_pem}' > ./myKey.pem"
   }
+*/
+}
+
+resource "local_file" "ssh_key" {
+  filename = "${aws_key_pair.kp.key_name}.pem"
+  content  = tls_private_key.pk.private_key_pem
 }
